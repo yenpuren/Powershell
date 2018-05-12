e7dc057416b403e9d512531c58ce41b7139dc33b
yenpuren <39217947+yenpuren@users.noreply.github.com>
2018-05-12 19:10:11 +0530

    PowerShell-master
    PowerShell-master

---
+++ PowerShell-master/Remove-SmtpAddress.ps1
@@ -0,0 +1,16 @@
+﻿#http://www.mikepfeiffer.net/2012/02/how-to-remove-e-mail-addresses-for-a-specific-domain-from-exchange/
+
+function Remove-SmtpAddress{
+    param(
+        $Domain
+    )
+
+    process {
+        foreach($i in Get-Mailbox -ResultSize Unlimited) {
+　         $i.EmailAddresses |
+　　　         ?{$_.AddressString -like "*@$domain"} | %{
+　　　　　         Set-Mailbox $i -EmailAddresses @{remove=$_}
+　　　         }
+        }
+    }
+}
